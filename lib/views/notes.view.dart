3e78b52297170a21d168bd831ee161e10e0b1dea
Lachgar Youssef <lachgar.youssef0@gmail.com>
2023-05-21 15:07:03 +0100

    8: i create Add Note Bottom Sheet and i affecte them to the floatButton

--- lib/views/notes.view.dart
+++ lib/views/notes.view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:notes_hive_app/views/widgets/add_note_bottom_sheet.view.dart';
 import 'package:notes_hive_app/views/widgets/notes_body.view.dart';
 
 class NotesView extends StatelessWidget {
@@ -10,7 +11,13 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return AddNoteBottomSheet();
+              });
+        },
         child: Icon(Icons.add),
       ),
       body: const NotesViewBody(),
